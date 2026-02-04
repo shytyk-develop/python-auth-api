0682e4885bdc6ebbd97432f9a324131c2f2fed86
Yan Shytyk <shytyk.develop@gmail.com>
2026-02-04 07:19:40 +0100

    Try one to fix vercel deployment

--- app/db/session.py
+++ app/db/session.py
@@ -1,13 +1,17 @@
 from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-
+from sqlalchemy.pool import NullPool
 from app.core.config import settings
 
 engine = create_async_engine(
     settings.database_url,
     echo=False,
     pool_pre_ping=True,
+    poolclass=NullPool,
+    connect_args={
+        "statement_cache_size": 0,
+        "prepared_statement_cache_size": 0,
+    }
 )
 
 AsyncSessionLocal = async_sessionmaker(
@@ -15,7 +19,6 @@ AsyncSessionLocal = async_sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
-
 
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
